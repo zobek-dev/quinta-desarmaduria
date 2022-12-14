bf935931ef4be5dfac23d7977ecaac6843ed5c08
zobek-dev <zobek.dev@gmail.com>
2022-12-14 17:19:19 -0300

    Avance de home

--- src/js/app.js
+++ src/js/app.js
@@ -30,13 +30,67 @@ class IconsCarousel extends HTMLElement{
       pagination: {
         el: this.pagination,
         clickable: true
-      }
-      
+      }  
     })
   }
 }
 
 customElements.define('icons-carousel', IconsCarousel)
+
+class DesarmeCarousel extends HTMLElement{
+  constructor(){
+    super();
+    this.carousel = this.querySelector('.desarme-carousel');
+    this.pagination = this.querySelector('.swiper-pagination');
+    this.next = this.querySelector('.swiper-button-next');
+    this.prev = this.querySelector('.swiper-button-prev');
+
+    this.swiper = new Swiper(this.carousel, {
+      modules: [Pagination, Navigation],
+      slidesPerView: 1.3,
+      spaceBetween: 8,
+      breakpoints: {
+        550: {
+          slidesPerView: 2.3
+        },
+        1024: {
+          slidesPerView: 3.3,
+          spaceBetween: 32,
+        }
+      },
+      navigation: {
+        nextEl: this.next,
+        prevEl: this.prev
+      },
+    })
+  }
+}
+customElements.define('desarme-carousel', DesarmeCarousel)
+
+class CategoryCarousel extends HTMLElement{
+  constructor(){
+    super();
+    this.carousel = this.querySelector('.categorias-carrousel');
+    this.swiper = new Swiper(this.carousel, {
+      slidesPerView: 1.3,
+      spaceBetween: 8,
+      breakpoints: {
+        550: {
+          slidesPerView: 2.3,
+          spaceBetween: 8,
+        },
+        1024: {
+          slidesPerView: 2.3,
+          spaceBetween: 16,
+        }
+      }
+    })
+  }
+}
+
+customElements.define('categorias-carousel', CategoryCarousel)
+
+
 
 
 // Auxiliar events
