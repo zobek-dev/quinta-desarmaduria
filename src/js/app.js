2e575141236936ddbdaa1919bab3360c6cdac988
zobek-dev <zobek.dev@gmail.com>
2022-12-14 12:01:59 -0300

    hero sin terminar, iconos finalizado

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,43 @@
 import Alpine from 'alpinejs';
+import Swiper, {Navigation, Pagination} from 'swiper';
 
 window.Alpine = Alpine;
 Alpine.start();
+
+// Swipers HTMLNative components
+
+class IconsCarousel extends HTMLElement{
+  constructor(){
+    super();
+    this.carousel = this.querySelector('.garantia-carousel');
+    this.pagination = this.querySelector('.swiper-pagination');
+
+    this.swiper = new Swiper(this.carousel, {
+      modules: [Navigation, Pagination],
+      slidesPerView: 1,
+      spaceBetween: 8,
+      breakpoints: {
+        550: {
+          slidesPerView: 2
+        },
+        750: {
+          slidesPerView: 3,
+        },
+        1024: {
+          slidesPerView: 4
+        }
+      },
+      pagination: {
+        el: this.pagination,
+        clickable: true
+      }
+      
+    })
+  }
+}
+
+customElements.define('icons-carousel', IconsCarousel)
+
 
 // Auxiliar events
 
